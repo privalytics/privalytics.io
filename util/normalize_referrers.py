1b4758536005d3b314b0803927e87346f4d4884e
Aquiles Carattino <aquiles@aquicarattino.com>
2019-02-28 11:50:06 +0100

    Screen size as bar plot, normalize referrers in own util

---
+++ util/normalize_referrers.py
@@ -0,0 +1,15 @@
+big_referrers = (
+            ('google.', 'Google'),
+            ('bing.com', 'Bing'),
+            ('twitter.com', 'Twitter'),
+            ('t.com', 'Twitter'),
+            ('reddit.com', 'Reddit')
+        )
+
+
+def normalize_referrer(referrer_url):
+    for big_ref in big_referrers:
+        if big_ref[0] in referrer_url:
+            return big_ref[1]
+
+    return referrer_url
