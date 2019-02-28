8920e358bf2834c87d0081be844a986c1965e211
Aquiles Carattino <aquiles@aquicarattino.com>
2019-02-28 10:25:53 +0100

    Improved tracker normalization URL

--- tracker/management/commands/normalize_website_tracks.py
+++ tracker/management/commands/normalize_website_tracks.py
@@ -1,18 +1,24 @@
+import logging
+
 from django.core.management import BaseCommand
 
 from tracker.models import Tracker
 from util.normalize_websites import normalize_website
 
 
+logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = """Checkes the raw logs and generates final trackers based on them
     """
 
     def handle(self, *args, **options):
-        trackers = Tracker.objects.all()
+        trackers = Tracker.objects.exclude(referrer_url=None).exclude(referrer_url='')
+        logger.info('Going to normalize {} referrers URLS'.format(trackers.count()))
         for tracker in trackers:
-            if tracker.referrer_url:
                 tracker.referrer_url = normalize_website(tracker.referrer_url)
+        trackers = Tracker.objects.exclude(url=None).exclude(url='')
+        logger.info('Going to normalize {} trackers urs'.format(trackers.count()))
+        for tracker in trackers:
             if tracker.url:
                 tracker.url = normalize_website(tracker.url)
             tracker.save()
