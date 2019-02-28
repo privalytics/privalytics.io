2151e23cc24682e2f8cbcbac7c33ec58a0480f15
Aquiles Carattino <aquiles@aquicarattino.com>
2019-02-28 07:01:57 +0100

    Management command tool to generate static pages

--- backend/management/commands/create_static_index.py
+++ backend/management/commands/create_static_index.py
@@ -9,9 +9,23 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        txt = render_to_string('privalytics/index.html')
-        if not os.path.exists('static_pages'):
-            os.makedirs('static_pages')
+        os.makedirs('static_pages')
+        os.makedirs('static_pages/privacy')
+        os.makedirs('static_pages/terms')
+        os.makedirs('static_pages/data-collection')
 
+        txt = render_to_string('privalytics/index.html')
         with open('static_pages/index.html', 'w') as f:
+            f.write(txt)
+
+        txt = render_to_string('privalytics/privacy.html')
+        with open('static_pages/privacy/index.html') as f:
+            f.write(txt)
+
+        txt = render_to_string('privalytics/terms.html')
+        with open('static_pages/terms/index.html') as f:
+            f.write(txt)
+
+        txt = render_to_string('privalytics/data_collection.html')
+        with open('static_pages/data-collection') as f:
             f.write(txt)
