d047b141e6e9a9ed37b8690a3fa42981bd793210
Aquiles Carattino <aqui.carattino@gmail.com>
2020-05-24 09:31:25 +0200

    Added page details

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('ws/<str:website_url>', WebsiteStats.as_view(), name='website'),
     path('ws/<str:website_url>/referrers', ReferrersView.as_view(), name='referrers'),
     path('ws/<str:website_url>/referrers/<str:ref_name>', ReferrerDetails.as_view(), name='referrer-details'),
-    path('ws/<str:website_url>/pages/<str:page_name>', PageDetails.as_view(), name='page-details'),
+    path('ws/<str:website_url>/pages/<path:page_name>', PageDetails.as_view(), name='page-details'),
 ]
